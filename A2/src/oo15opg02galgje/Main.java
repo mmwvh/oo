6a3a9b81d58ac99f01d9a20055ca3333dee12127
mmwvh <wvhoorn92@gmail.com>
2015-02-11 21:25:19 +0100

    De benodigde klassen onderverdeelt in packages

---
+++ A2/src/oo15opg02galgje/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Object Orientation Artificial Intelligence
+ * 
+ * @author Franka Buytenhuijs / s4356845
+ * @author Wesley van Hoorn / s4018044
+ * 
+ **/
+
+package oo15opg02galgje;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ A2/src/oo15opg02interactie/View.java
@@ -0,0 +1,13 @@
+/**
+ * Object Orientation Artificial Intelligence
+ * 
+ * @author Franka Buytenhuijs / s4356845
+ * @author Wesley van Hoorn / s4018044
+ * 
+ **/
+
+package oo15opg02interactie;
+
+public class View {
+
+}
