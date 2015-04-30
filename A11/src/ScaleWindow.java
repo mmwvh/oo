3ee44a9274b15f04131f10828a2eb563397fd448
mmwvh <wvhoorn92@gmail..com>
2015-04-30 14:57:42 +0200

    Coole text

--- A11/src/ScaleWindow.java
+++ A11/src/ScaleWindow.java
@@ -8,46 +8,50 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-
-public class ScaleWindow implements ActionListener{
-	public static final int WIDTH = 200, HEIGHT = 200;
+public class ScaleWindow implements ActionListener  {
+	public static final int WIDTH = 200, HEIGHT = 120;
 	private JTextField scalefactor;
 	private JTextField xcenter;
 	private JTextField ycenter;
+	private TextPrompt scaletext;
+	private TextPrompt xtext;
+	private TextPrompt ytext;
 	private JButton b;
 	private GridFiller gf;
-	
-	public ScaleWindow (GridFiller gf) {
-		
-		this.gf =gf;
-    	JFrame scaleFrame = new JFrame ("Scale");
-        
-    	scaleFrame.setSize (WIDTH, HEIGHT);                
-    	scaleFrame.setLocationRelativeTo(null);
-    	scaleFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	scaleFrame.setResizable(false);
-    	scaleFrame.setLayout(new BorderLayout());
-    	
-    	b = new JButton("Apply");
-    	b.setMinimumSize(new Dimension(50, 50));
-    	scaleFrame.add(b, BorderLayout.SOUTH);
-    	b.addActionListener(this);
-    	
-    	JPanel panel = new JPanel();
-    	panel.setLayout(new BorderLayout());
-    	scaleFrame.add(panel, BorderLayout.NORTH);
-    	
-    	scalefactor = new JTextField("Scalefactor");
-    	ycenter = new JTextField("y coordinate of the center");
-    	xcenter = new JTextField("x coordinate of the center");
-    	panel.add(scalefactor, BorderLayout.NORTH);
-    	panel.add(ycenter, BorderLayout.SOUTH);
-    	panel.add(xcenter, BorderLayout.CENTER);
-    	panel.setVisible(true);
-    	scaleFrame.setVisible(true);
-    	
-    	
-}
+
+	public ScaleWindow(GridFiller gf) {
+
+		this.gf = gf;
+		JFrame scaleFrame = new JFrame("Scale");
+
+		scaleFrame.setSize(WIDTH, HEIGHT);
+		scaleFrame.setLocationRelativeTo(null);
+		scaleFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		scaleFrame.setResizable(false);
+		scaleFrame.setLayout(new BorderLayout());
+
+		b = new JButton("Apply");
+		b.setMinimumSize(new Dimension(50, 50));
+		scaleFrame.add(b, BorderLayout.SOUTH);
+		b.addActionListener(this);
+
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		scaleFrame.add(panel, BorderLayout.NORTH);
+
+		scalefactor = new JTextField();
+		scaletext = new TextPrompt("Scale factor", scalefactor);
+		ycenter = new JTextField();
+		ytext = new TextPrompt("Y coordinate of the center", ycenter);
+		xcenter = new JTextField();
+		xtext = new TextPrompt("X coordinate of the center", xcenter);
+		panel.add(scalefactor, BorderLayout.NORTH);
+		panel.add(ycenter, BorderLayout.SOUTH);
+		panel.add(xcenter, BorderLayout.CENTER);
+		panel.setVisible(true);
+		scaleFrame.setVisible(true);
+
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -55,11 +59,8 @@ public class ScaleWindow implements ActionListener{
 		double y = Double.parseDouble(ycenter.getText());
 		double x = Double.parseDouble(xcenter.getText());
 
-		
-		gf.setScale(x,y,sf);
-		
+		gf.setScale(x, y, sf);
 
 	}
 
-	
 }
